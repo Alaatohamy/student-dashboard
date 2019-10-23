0ed319bbdb1b82788ad082b49b64d84c3cb59160
Alaatohamy <alaatohamy4@gmail.com>
2019-10-23 02:34:01 +0200

    Updates

---
+++ precache-manifest.b6ebeb16f7f33156737f73bf2c6efad7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f71c3596d9b61b9082c0e974e199b794",
+    "url": "/student-dashboard/index.html"
+  },
+  {
+    "revision": "3fdd033c8894bd1bafba",
+    "url": "/student-dashboard/static/css/main.4a247d9c.chunk.css"
+  },
+  {
+    "revision": "f56dd866ac954e7ca7bb",
+    "url": "/student-dashboard/static/js/2.6b918826.chunk.js"
+  },
+  {
+    "revision": "3fdd033c8894bd1bafba",
+    "url": "/student-dashboard/static/js/main.47702547.chunk.js"
+  },
+  {
+    "revision": "1fe2cfc9457fb4d9ae09",
+    "url": "/student-dashboard/static/js/runtime-main.82fb1f5c.js"
+  },
+  {
+    "revision": "9fbaaf7c02237e88cc8e94d5aae5e829",
+    "url": "/student-dashboard/static/media/calendar.9fbaaf7c.svg"
+  },
+  {
+    "revision": "daa9a3f145c1e347159f9a617d2354d3",
+    "url": "/student-dashboard/static/media/email.daa9a3f1.svg"
+  },
+  {
+    "revision": "57a25d29ddcbf7ec436de8ce6e3f68b2",
+    "url": "/student-dashboard/static/media/event.57a25d29.svg"
+  },
+  {
+    "revision": "21ebacbaf3062604a707188078debf6a",
+    "url": "/student-dashboard/static/media/file.21ebacba.svg"
+  },
+  {
+    "revision": "fba6f52a4dc542f19ac655c7ea5487f5",
+    "url": "/student-dashboard/static/media/graduate.fba6f52a.svg"
+  },
+  {
+    "revision": "a1a27a986a73afdf3b0792f83fabba5f",
+    "url": "/student-dashboard/static/media/highlight-background.a1a27a98.png"
+  },
+  {
+    "revision": "a46bb50a66bb8a137923151c192ed30c",
+    "url": "/student-dashboard/static/media/home.a46bb50a.svg"
+  },
+  {
+    "revision": "66827f9f6893b3b8a63ab038d4a9731c",
+    "url": "/student-dashboard/static/media/notification.66827f9f.svg"
+  },
+  {
+    "revision": "6f70b9c8758f87151a241042daa621bf",
+    "url": "/student-dashboard/static/media/profits.6f70b9c8.svg"
+  },
+  {
+    "revision": "2bbf1aabf329c0bb20ac348c4c39e6b6",
+    "url": "/student-dashboard/static/media/sands-of-time.2bbf1aab.svg"
+  },
+  {
+    "revision": "4c288b474fb24a3a8c45dfd97cd4e156",
+    "url": "/student-dashboard/static/media/speaker.4c288b47.svg"
+  },
+  {
+    "revision": "5e946c20186e33b608fbb3388fff6ff5",
+    "url": "/student-dashboard/static/media/user.5e946c20.svg"
+  }
+]);
